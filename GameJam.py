b8dbf4a22d71f0b1d7c3026c44b3adf295ffc8cd
Janay S <145382048+JanayLS@users.noreply.github.com>
2024-03-06 12:31:24 -0500

    Add files via upload

---
+++ GameJam.py
@@ -0,0 +1,4 @@
+users_name=input("Hey, what's your name?: ")
+print(f"Welcome {users_name}! \nYou are at currently at the train station. "
+      f"\nYou have 15 min before you're train arrives. "
+      f"\nThe train you're about to take is headed for Bloomsbury Park, NC. ")
