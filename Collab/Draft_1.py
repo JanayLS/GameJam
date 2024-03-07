680b14675633428d5b927ea80cd4deaebffcc965
aliWTC <144477317+aliWTC@users.noreply.github.com>
2024-03-07 12:56:10 -0500

    Update Draft_1.py
    Completed the inventory functionality, iterated through inventory to return inventory amount

--- Collab/Draft_1.py
+++ Collab/Draft_1.py
@@ -2,21 +2,22 @@
 capital = 50
 
 #status
-knife = 0
-bandages = 0
-food = 4
-water = 2
-battery_pack = 0
-crowbar = 0
+inventory = [0, 0, 0, 0, 0, 0]
+
+
+
+purchased_items = []
+
 
 #Gas station options with prices
 options = {'knife':'16', 'bandage':'10', 'snack':'5', 'battery':'11', 'crowbar':'20', "water":'2'}
+options_list = ['knife', 'bandages', 'snack', 'battery', 'crowbar', 'water']
 
-
+print(options['knife'])
 
 #to print the menu without brackets:
 #for loop to get the keys and values of the dictionary
-def gas_station(choices, money):
+def gas_station(choices, money, inventory):
 
     print("Gas Station Menu:")
     print("-----------------------")
@@ -25,25 +26,63 @@ def gas_station(choices, money):
     print("-----------------------")
 
 
-
     item_choice = input("What would you like to buy? (q to quit): ")
-    money -= int(options[item_choice])
-    print(f'Remaining money: ${money}')
+
+    if item_choice != 'q':
+
+        money -= int(options[item_choice])
+        print(f'Remaining money: ${money}')
+        purchased_items.append(item_choice)
 
     while item_choice != 'q':
         item_choice = input("What would you like to buy? (q to quit): ")
 
+        if item_choice == 'q':
+            break
+
+
+        if item_choice not in choices:
+            print("Item not on menu, please try again")
 
 
         if int(options[item_choice]) > money:
             print("Not enough money")
 
+        if int(options[item_choice]) == money:
+            money -= int(choices[item_choice])
+            print(f"Remaining money: ${money}")
+            purchased_items.append(item_choice)
 
 
         elif int(options[item_choice]) < money:
             money -= int(choices[item_choice])
             print(f"Remaining money: ${money}")
+            purchased_items.append(item_choice)
 
 
 
-gas_station(options, capital)
+
+
+
+
+    print("You've left the gas station. ")
+    print()
+    print()
+
+
+    for i in purchased_items:
+        if i in options_list:
+            inventory[options_list.index(i)] += 1
+
+    print('INVENTORY')
+    print("-----------------------")
+    printed = []
+    for i in purchased_items:
+        if i in printed:
+            continue
+        else:
+            print(f'{i}:{purchased_items.count(i)}')
+            printed.append(i)
+
+
+gas_station(options, capital, inventory)
